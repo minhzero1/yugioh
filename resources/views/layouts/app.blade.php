eebe08da61f57b7c4a4a90aa06e60192f9cff491
minhzero1 <minhzero2005@gmail.com>
2026-07-06 08:51:01 +0700

    Cap nhat background dong va layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,14 @@
 </head>
 <body>
 
+    {{-- Hiệu ứng hạt sáng trôi, thuần CSS, không dùng JS/thư viện ngoài --}}
+    <div class="particle" style="width:4px; height:4px; left:10%; animation-duration:18s; animation-delay:0s;"></div>
+    <div class="particle" style="width:3px; height:3px; left:25%; animation-duration:22s; animation-delay:3s;"></div>
+    <div class="particle" style="width:5px; height:5px; left:45%; animation-duration:16s; animation-delay:6s;"></div>
+    <div class="particle" style="width:3px; height:3px; left:65%; animation-duration:25s; animation-delay:1s;"></div>
+    <div class="particle" style="width:4px; height:4px; left:80%; animation-duration:20s; animation-delay:4s;"></div>
+    <div class="particle" style="width:3px; height:3px; left:92%; animation-duration:19s; animation-delay:8s;"></div>
+
     @include('partials.nav')
 
     @yield('content')
